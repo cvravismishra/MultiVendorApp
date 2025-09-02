25318b18744b3e4ddfab2636e483be20a386dd02
cvravismishra <cvravismishra@gmail.com>
2025-09-02 00:30:42 -0700

    Add files via upload

---
+++ storage_app/src/test/java/com/amazon/storage_app/StorageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.storage_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
